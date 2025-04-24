a3689bc630158e28909ebb70be0758fe6840ee00
Alexander Reviakin <reviakin@gmail.com>
2025-04-24 12:09:13 +0500

    v1.0

--- lib_powercli.ps1
+++ lib_powercli.ps1
@@ -1,4 +1,4 @@
-﻿$lib_version="0.8.2powercli"
+﻿$lib_version="1.0powercli"
 $VMware_vendor="VMware"
 $script:VMware_ESXi="$($VMware_vendor) ESXi"
 
@@ -33,7 +33,7 @@ $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 
 
 
-#запись данных о пользователе в БД
+#запись данных о ESXI гипервизоре в БД
 function pushVMHostData() {
 	param
 	(
@@ -44,31 +44,14 @@ function pushVMHostData() {
 	#если имя ноды - IP адрес, то выдергиваем hostname
 	if ($VMhost.name -match "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$") {
 		$strComp=$VMhost.NetworkInfo.Hostname
-		$strDomain=$VMhost.NetworkInfo.DomainName.split('.')[0]
 		$strFullName="$strComp.$($VMhost.NetworkInfo.DomainName)"
 	} else {
 		$strComp=$VMhost.name.split('.')[0]
-		$strDomain=$VMhost.name.split('.')[1]
 		$strFullName=$VMhost.name
 	}
 
 	#Write-Host -ForegroundColor Yellow $strFullName
-	
-	#Ищем домен
-	$intDomain=getInventoryId 'domains' $strDomain
-	if ([int]$intDomain -lt 0) {
-		#не нашли
-		errorLog "ESXi host domain not found: $($strFullName)"
-		return
-	}
 
-	#Ищем OS
-	$intComp=getInventoryId 'comps' "$($strDomain)/$($strComp)"
-	if ([int]$intComp -le 0) {
-		#не нашли
-		errorLog "ESXi OS not found: $($strFullName)"
-		return
-	}
 
 	#write-host -ForegroundColor Green $intComp
 
@@ -86,7 +69,7 @@ function pushVMHostData() {
 
 	$strMem= -join('{"memorybank": {',
 		'"manufacturer":"', $VMhost.Manufacturer ,'",',
-		'"capacity":"', [string]($VMHost.MemoryTotalGb*1024) ,'",',
+		'"capacity":"', [string]([math]::Round($VMHost.MemoryTotalMb)) ,'",',
 		'"serial":""',
 	'}}')
 
@@ -103,50 +86,43 @@ function pushVMHostData() {
 		}
 	}
 
+	$uuid=getVMhostUUID $VMHost
 	#$vmhost
-	$data= -join(
-		"domain_id=$($intDomain)&",
-		"name=$([System.Web.HttpUtility]::UrlEncode($strComp)) &",
-		"os=$([System.Web.HttpUtility]::UrlEncode($strOS))&",
-		"raw_hw=$([System.Web.HttpUtility]::UrlEncode("$($strMb),$($strCpu),$($strMem)"))&" ,
-		"raw_soft=&",
-		"raw_version=$([System.Web.HttpUtility]::UrlEncode($lib_version))&",
-		"ip=$([System.Web.HttpUtility]::UrlEncode($strIfaces))&",
-		"mac=$([System.Web.HttpUtility]::UrlEncode($strMACs))&",
-		"updated_at=$([System.Web.HttpUtility]::UrlEncode($strNow))"
-	)
-	#$data
-	$result=setInventoryData 'comps' $intComp $data
-	if ($intComp -gt 0) {
-		$method="Updating (id $intComp)"
-	} else {
-		$method="Creating new"
+	$data= @{
+		name=[System.Web.HttpUtility]::UrlEncode($strFullName);
+		os=[System.Web.HttpUtility]::UrlEncode($strOS);
+		raw_hw=[System.Web.HttpUtility]::UrlEncode("$($strMb),$($strCpu),$($strMem)");
+		raw_soft="";
+		raw_version=[System.Web.HttpUtility]::UrlEncode($lib_version);
+		ip=[System.Web.HttpUtility]::UrlEncode($strIfaces);
+		mac=[System.Web.HttpUtility]::UrlEncode($strMACs);
+		external_links=$(@{'VMWare.hostUUID'=$uuid}|ConvertTo-Json);
 	}
+	#$data
+	$result=pushInventoryData 'comps' $data
+
 	#$result | fl
 	if ($result) {
-		Log "ESXi host $($strFullName) - OK ($method)"
+		spooLog "ESXi host $($strFullName) - OK"
 	} else {
 		errorLog "ESXi host $($strFullName) - Error ($method)"
-		Write-Host -ForegroundColor Cyan "domain_id=$intDomain //$strDomain"
-		Write-Host -ForegroundColor Cyan "name=$strComp"
+		Write-Host -ForegroundColor Cyan "name=$strFullName"
 		Write-Host -ForegroundColor Cyan "os=$strOS"
 		Write-Host -ForegroundColor Cyan "raw_hw=`"$strMb,$strCpu,$strMem`""
 		Write-Host -ForegroundColor Cyan "raw_soft="
 		Write-Host -ForegroundColor Cyan "raw_version=$lib_version"
 		Write-Host -ForegroundColor Cyan "ip=$strIfaces"
 		Write-Host -ForegroundColor Cyan "mac=$strMACs"
-		Write-Host -ForegroundColor Cyan "updated_at=$strNow"
-	   
 	}
 	return
 }
 
 
-
 function pushVMData() {
 	param
 	(
-		$VM
+		[object]$VM,
+		[object]$objComp=$null
 	)
 	#Log "Parsing $($VM.Name) $($VM.Id)"
 	#запрос дополнительной информации недоступной через Get-VM (нам нужно число ядер на сокет)
@@ -156,64 +132,46 @@ function pushVMData() {
 	# 1. Экранировать служебные для регекспа символы
 	# 2. Явно обозначать что мы ищем ^name$ (^-начало строки, а $ - конец) иначе находятся name2, name_clone и т.п.
 	#$VMView=$VM|Get-View # -ViewType VirtualMachine
-	$VMView=Get-View $VM
-	$VMGuest=Get-VMGuest $VM #.name | select vmName,Hostname,OSFullName,GuestFamily,Disks,IPAddress
+	#$VMView=Get-View $VM
+	#$VMGuest=Get-VMGuest $VM #.name | select vmName,Hostname,OSFullName,GuestFamily,Disks,IPAddress
+	$VMGuest=getCachedVmGuest($VM);
 	#$VMGuest
 	#$VMView.config.hardware
-	#$strMb= "{`"motherboard`":{`"manufacturer`":`"$($VMhost.Manufacturer)`",`"product`":`"$($VMhost.Model)`",`"serial`":`"`"}}"
-	$strCpu="{`"processor`": {`"model`":`"$($VMware_vendor)`",`"cores`":`"$($VM.NumCpu)`"}}"
-	#$strCpu="{`"processor`": {`"model`":`"$($VMware_vendor)`",`"cores`":`"$($VM.NumCpu*$VMView.config.hardware.NumCoresPerSocket)`"}}"
-	$strMem="{`"memorybank`": {`"manufacturer`":`"$($VMware_vendor)`",`"capacity`":`"$($VM.MemoryGB*1024)`",`"serial`":`"`"}}"
-	$strDisks=""
-	foreach ($VMDisk in $VM.Guest.disks) {
-		$strdisks="$($strDisks),{`"harddisk`":{`"model`":`"VMware Virtual disk SCSI Disk Device`",`"size`":`"$([math]::Round($VMDisk.CapacityGB))`"}}"
-	}
-	$strOS=$VMGuest.OSFullName
+	$uuid=getVMUUID $VM;
 	#если у нас есть хостнейм (#бывает и нулл)
 	if (!$VMGuest.Hostname) {
-		errorLog "VM: $($VM.name) no hostname"
+		errorLog "VM: $($hostname) ($($VM.Name) : $($uuid)) no hostname"
 		return
 	}
 
-	#вытаскиваем из всех IP адресов только IPv4
-	$arrIPv4=@();
-	foreach ($IP in $VMGuest.IPAddress) {
-		if ($IP -match "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$") {$arrIPv4 += $IP}
-	}
-
-	if ($VMGuest.HostName.split('.').count -gt 1 ) {
-		$strComp=$VMGuest.HostName.split('.')[0]
-		$strDomain=$VMGuest.HostName.split('.')[1]
-		$intDomain=getInventoryId 'domains' $strDomain
-		if ([int]$intDomain -lt 0) {
-			errorLog "VM: $($strDomain)\$($strComp) domain not found in Inventory"
-			return
-		}
+	$strFullName=$VMGuest.HostName;
+	if ($strFullName.split('.').count -gt 1 ) {
+		$strComp=$strFullName.split('.')[0]		
 	} else {
-		$strComp=$VMGuest.HostName
+		$strComp=$strFullName
 		#warningLog "VM: $($strComp) ($($arrIPv4 -join " ")) no DOMAIN in ESXi"
 		#если домена нет. будем искать по связке имя+IP
 		#Write-Host -foregroundColor Yellow "HOST DOMAIN NOT FOUND, Searching by name and ip: " , name, ($VMGuest.IPAddress -join " ")
-		#Write-Host -foregroundColor Yellow "name=$($strComp)&ip=$($VMGuest.IPAddress -join " ")"
-			
-		$objComp=getInventoryObj 'comps' "search?name=$($strComp)&ip=$($arrIPv4 -join ' ')"
+		#Write-Host -foregroundColor Yellow "name=$($strFullName)&ip=$($VMGuest.IPAddress -join " ")"
+		if ( -not $objComp) {
+			$objComp=getInventoryObj 'comps' "$($strFullName)&ip=$($arrIPv4 -join ' ')&expand=domain"
+			#$objComp
+		}
 
 		#если нашли, то вытаскиваем имя домена
 		if ($objComp) {
-			$intDomain=$objComp.domain_id
-			$objDomain=getInventoryObj 'domains' $intDomain
-			$strdomain=$objDomain.name
-			warningLog "VM: $($strDomain)\$($strComp) no DOMAIN in ESXi //found in inventory by IP($($arrIPv4 -join " "))"
+			$strdomain=$objComp.domain.fqdn
+			$strFullName="$($strFullName).$($strDomain)";
+			warningLog "VM: $strFullName no FQDN hostname in ESXi //found in inventory by IP($($arrIPv4 -join " "))"
 			#Write-Host -foregroundColor Green "Found Domain:",$intDomain
 		} else {
 			#Write-Host -foregroundColor Red "Domain not found, using default: ", $inventory_defaultDomain
-			errorLog "VM: $($strComp) ($($arrIPv4 -join ' ')) no DOMAIN in ESXi (can't create without domain)"
+			errorLog "VM: $($strComp) ($($_.Name) : $($uuid) : $($arrIPv4 -join ' ')) нет FQDN в VMWare (без домена не могу добавить в инвентори)"
 			return
-			#$strDomain=$inventory_defaultDomain
-			#$intDomain=getInventoryId 'domains' $inventory_defaultDomain
 		}
 	}
 
+	#spooLog "VM parsing $($strFullName)..."
 	#Ищем, а есть ли уже в базе наш ESXi хост и есть ли у него АРМ?
 	if ($VM.VMhost.name -match "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$") {
 		$strHost="$($VM.VMhost.NetworkInfo.Hostname).$($VM.VMhost.NetworkInfo.DomainName)"
@@ -221,58 +179,108 @@ function pushVMData() {
 		$strHost=$VM.VMHost.name
 	}
 	
-	$objESXi=getInventoryFqdnComp $strHost
-	if ($objESXi -and $objESXi.arm_id) {
+	$objESXi=getInventoryFqdnComp $strHost;
+	if ($objESXi -and ($objESXi.arm_id -gt 0)) {
 		#Все есть! Круть!
-		$intArm=$objESXi.arm_id
+		$intArm=[int]($objESXi.arm_id);
 	} else {
 		#нету ножек - нет варенья
 		$intArm=$false
-		errorLog "VM: no ESXi host $strHost found in Inventory"
+		errorLog "HOST: $($strHost) без АРМ в инвентори!"
 	}
 
-		
-	$strNow=(Get-Date).ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss")
-	$strIfaces=$arrIPv4 -join "`n"
-	$strIfacesMacs=$VMGuest.MACAddress -join "`n"
+	#$objComp;	
+
+	#spooLog "parsing $($strFullName) ..."
+    
 
 	#тут короче такая логика, мы узнаем, есть ли эта машина в базе
 	#если она есть, то мы смотрим, есть ли по ней данные от скрипта из ОС (более полный набор)
 	#если есть, то мы просто помещаем ее в тот, АРМ, на хосте которого она крутится
 	#если нет, то мы полностью отправляем информацию о ней
-	$objComp=getInventoryObj 'comps' "$($strDomain)/$($strComp)"
+	if ( -not $objComp) {
+        	debugLog "getting $($strFullName) from inventory"
+		$objComp = getInventoryFqdnComp $strFullName
+	}
 
-	$data=-join(
-		"domain_id=$($intDomain)&",
-		"name=$([System.Web.HttpUtility]::UrlEncode($strComp))&",
-		"os=$([System.Web.HttpUtility]::UrlEncode($strOS))&",
-		"raw_hw=$([System.Web.HttpUtility]::UrlEncode("$($strCpu),$($strMem)$($strDisks)"))&",
-		"ignore_hw=1&",
-		"raw_soft=&",
-		"raw_version=$([System.Web.HttpUtility]::UrlEncode($lib_version))&",
-		"ip=$([System.Web.HttpUtility]::UrlEncode($strIfaces))&",
-		"mac=$([System.Web.HttpUtility]::UrlEncode($strIfacesMacs))&",
-		"updated_at=$([System.Web.HttpUtility]::UrlEncode($strNow))"
-	)
+
+	#если компа нет, или он есть, но 
+	#скрипт обновлявший его содержит префикс powercli 
+	#или нет скрипта
+	if (    `		
+		(-not ($objComp -is [Object])) `
+		-or `
+		(($objComp -is [Object]) -and (`
+			([string]($objComp.raw_version)).contains("powercli")`
+			-or `
+			(([string]($objComp.raw_version)).trim().Length -eq 0)`
+		))`
+	) {
+		$strCpu="{`"processor`": {`"model`":`"$($VMware_vendor)`",`"cores`":`"$($VM.NumCpu)`"}}"
+		$strMem="{`"memorybank`": {`"manufacturer`":`"$($VMware_vendor)`",`"capacity`":`"$($VM.MemoryGB*1024)`",`"serial`":`"`"}}"
+		$strDisks=""
+		foreach ($VMDisk in $VM.Guest.disks) {
+			$strdisks="$($strDisks),{`"harddisk`":{`"model`":`"VMware Virtual disk SCSI Disk Device`",`"size`":`"$([math]::Round($VMDisk.CapacityGB))`"}}"
+		}
+		$strOS=$VMGuest.OSFullName
+        if ( -not $strOS ) {
+			errorLog "VM: $($strComp) ($($_.Name) : $($uuid) : $($arrIPv4 -join ' ')) нет типа ОС (Linux/Windows) в VMWare (без него не могу добавить в инвентори, обнови VMWare tools)"
+			return
+        }
+
+		#вытаскиваем из всех IP адресов только IPv4
+		$arrIPv4=@();
+		foreach ($IP in $VMGuest.IPAddress) {
+			if ($IP -match "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$") {$arrIPv4 += $IP}
+		}
+
+		$arrMacs=@();
+		foreach ($Nic in $VMGuest.Nics) {
+			$arrMacs += $Nic.MacAddress;
+		}
+
+		$strIfaces=$arrIPv4 -join "`n"
+		$strIfacesMacs=$arrMacs -join "`n"
+		$data=@{
+			name=[System.Web.HttpUtility]::UrlEncode($strFullName);
+			os=[System.Web.HttpUtility]::UrlEncode($strOS);
+			raw_hw=[System.Web.HttpUtility]::UrlEncode("$($strCpu),$($strMem)$($strDisks)");
+			ignore_hw=1;
+			raw_soft="";
+			raw_version=[System.Web.HttpUtility]::UrlEncode($lib_version);
+			ip=[System.Web.HttpUtility]::UrlEncode($strIfaces);
+			mac=[System.Web.HttpUtility]::UrlEncode($strIfacesMacs);
+			external_links=$(@{'VMWare.UUID'=$uuid}|ConvertTo-Json);
+		}
+	} else {
+		$data=@{
+			ignore_hw=1;
+			external_links=$(@{'VMWare.UUID'=$uuid}|ConvertTo-Json);
+		}
+	}
+
+	#если знаем ОС в инвентори
+	if ($objComp) {
+		$data["id"]=$objComp.id;
+	} 
 
 	#если нашли текущий АРМ - закрепляем
 	if ($intArm) {
-		$data="$($data)&arm_id=$($intArm)"
+		$data["arm_id"]=$intArm;
 	} 
 
+	#$data
+
 	#ОС есть в БД, она обновлена не этим скриптом и мы знаем в какой АРМ ее положить
-	$logPrefix="VM: $($strDomain)\$($strComp)".PadRight(30," ")
-	if ($objComp -and $intArm -and ( -not $objComp.raw_version.contains("powercli"))) {
-		$data="arm_id=$($intArm)&ignore_hw=1"
-		setInventoryData 'comps' $objComp.id $data | out-null
+	$logPrefix="VM: $($strFullName)".PadRight(30," ")
+	if ($objComp -and $intArm -and ( -not  ([string]($objComp.raw_version)).contains("powercli"))) {
+		setInventoryData 'comps' $data | out-null
 		spooLog "$logPrefix - OK (arm update only)"
 	} elseif ($objComp) {
-		#Write-Host Updating $VMGuest.HostName
-		setInventoryData 'comps' $objComp.id $data | out-null
+		setInventoryData 'comps' $data | out-null
 		spooLog "$logPrefix - OK (update)"
 	} elseif ($strIfaces.Length -ge 7) {
-		#Write-Host Missing $VMGuest.HostName
-		setInventoryData 'comps' -1 $data | out-null
+		pushInventoryData 'comps' $data | out-null
 		spooLog "$logPrefix - OK (new OS)"
 	} else {
 		spooLog "$logPrefix - Skipped (new VM without IP)"
@@ -280,85 +288,172 @@ function pushVMData() {
 }
 
 
-function parseVCenter() {
+function connectVCenter() {
 	param
 	(
 		[string]$VcenterServer,
 		[string]$VcenterUser,
 		[string]$VcenterPassword
 	)
-
-	Log "Connecting $($VcenterServer) ..."
+	spooLog "Connecting $($VcenterServer) ... "
 	$Server= Connect-VIServer -server $VcenterServer -protocol https -User $VcenterUser -Password $VcenterPassword -force
 	if (!$Server -or !$Server.isConnected) {
 		errorLog "VSphere $($VcenterServer) connetion error"
 		return
 	}
-	Log "OK"
-	
-	Log "Loading VMHosts ..."
-	#собираем хосты
-	$VMhosts = Get-VMHost | select name,ProcessorType,NumCpu,Manufacturer,Model,MemoryTotalMB,Version,NetworkInfo,ConnectionState
-	Log "OK"
-
-	Log "Loading VMHosts interfaces ..."
-	#собираем интерфейсы:
-	$VMifaces = Get-VMHostNetworkAdapter | select VMhost, IP, MAC
-	Log "OK"
-
-	Log "Loading VMs ..."
-	$VMs = get-VM #| select name,PowerState,NumCpu,MemoryGb,ProvisionedSpaceGB,Guest,VMHost,Id
-	Log "OK"
-	
-	Log "Parsing Online VMHosts..."
-	foreach ($VMhost in $VMhosts)  {
-		if ($VMhost.ConnectionState -eq 'Connected') {
-			pushVMHostData $VMhost $VMifaces
-		}
-	}
-	Log "OK"
-
-	Log "Parsing Online VMs..."
-	foreach( $VM in $VMs) {
-		#$VM
-		if ($VM.PowerState -eq 'PoweredOn' ) {
-			pushVMData $VM
-		}
-	}
-	Log "OK"
-
-	Disconnect-VIServer -server $Server -force  -confirm:$false |Out-Null
-	Log "Vcenter server $($VcenterServer) complete."
+	spooLog "OK"
 }
 
-function parseVcenterVm() {
+#грузим VM если она включена и не спит (ОС запущена, стутус vmtools - running)
+function loadVM($VM) {
+    if ( -not $VM) {
+        return;
+    }
+	if ($VM.PowerState -eq 'PoweredOn' ) {
+        $uuid=getVMUUID $VM;
+    
+        if ( $($vm | Get-HardDisk).Count -eq 0 ) {
+            spooLog "VM: $($VM.Name) : $($uuid) : no HDD in VM - skip inventorying"
+            return;
+        }
+
+        $VMGuest=Get-VMGuest $VM;
+        if ($VMGuest -and ($VMGuest.State -eq 'Running')) {
+	    	$global:VMLIST+=$VM;
+		    $global:VMGuests[$uuid]=Get-VMGuest $VM;
+        } else {
+            errorLog "VM: $($VM.Name) : $($uuid) : VMTools not running  ($($VMGuest.State))"
+        }
+	} else {
+		debugLog "Powered off ($($VM.PowerState))"
+	}
+}
+
+function parseVCenter() {
 	param
 	(
-		[string]$VcenterServer,
-		[string]$VcenterUser,
-		[string]$VcenterPassword,
-		[string]$VmName
+		[string]$VmName=''
 	)
+	if ($VmName.length) {
+		spooLog "Loading VM $($VmName)..."
+		loadVM (get-VM $VmName)
+		$VM=get-VM $VmName;
+		spooLog "OK"
 
-	Log "Connecting $($VcenterServer) ..."
-	$Server= Connect-VIServer -server $VcenterServer -protocol https -User $VcenterUser -Password $VcenterPassword -force
-	if (!$Server -or !$Server.isConnected) {
-		errorLog "VSphere $($VcenterServer) connetion error"
-		return
-	}
-	Log "OK"
-	
-	
-	$VM = get-VM $VmName
-	
-	if ($VM.PowerState -eq 'PoweredOn' ) {
-		Log "Parsing $VmName ..."
-		pushVMData $VM
-		Log "OK"
+		#spooLog "Loading VMHost $($VM.VMHost.name)..."
+		#$VMhosts = Get-VMHost $VM.VMHost.name | Select-Object name,ProcessorType,NumCpu,Manufacturer,Model,MemoryTotalMB,Version,NetworkInfo,ConnectionState,Uid
+		#spooLog "OK"
+
 	} else {
-		Log "Powered off ($($VM.PowerState))"
+		spooLog "Getting Online VMs..."
+		foreach( $VM in (get-VM)) {
+			loadVM($VM);
+		}
+		spooLog "OK"
+
+		spooLog "Loading VMHosts ..."
+		#собираем хосты
+		$VMhosts = Get-VMHost | Select-Object name,ProcessorType,NumCpu,Manufacturer,Model,MemoryTotalMB,Version,NetworkInfo,ConnectionState,Uid
+		spooLog "OK"
+
+		spooLog "Loading VMHosts interfaces ..."
+		#собираем интерфейсы:
+		$VMifaces = Get-VMHostNetworkAdapter | Select-Object VMhost, IP, MAC
+		spooLog "OK"
+
+		spooLog "Parsing Online VMHosts..."
+		foreach ($VMhost in $VMhosts)  {
+			if ($VMhost.ConnectionState -eq 'Connected') {
+				pushVMHostData $VMhost $VMifaces
+			}
+		}
+		spooLog "OK"
 	}
 
-	Disconnect-VIServer -server $Server -force  -confirm:$false |Out-Null
-	Log "Vcenter server $($VcenterServer) complete."
+
+
+
+
+	#Если сделать дисконнект, то сохраненные в памяти переменные не смогут обращаться к связанным объектам.
+	#Например обращение $VM.VMHost дает ничего, если коннект бн
+	#Disconnect-VIServer -server $Server -force  -confirm:$false |Out-Null
+	spooLog "Vcenter server $($VcenterServer) complete."
+}
+
+#Найти в кэше гостевую ОС для этой ВМ 
+function getCachedVmGuest() {
+	param($VM)
+    $uuid=getVMUUID $VM;
+	return $global:VMGuests[$uuid];
+}
+
+#получить Hostname для этой ВМ
+function getVMHostname() {
+	param($VM)
+	return $(getCachedVmGuest $VM).HostName;
+}
+
+#получить IP адреса для этой ВМ
+function getVMIps() {
+	param($VM)
+	$VMGuest=getCachedVmGuest $VM;
+
+	$arrIPv4=@();
+	foreach ($IP in $VMGuest.IPAddress) {
+		if ($IP -match "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$") {$arrIPv4 += $IP}
+	}
+
+    return $arrIPv4;
+}
+
+
+
+
+#посчитать 
+function countVMsHostname() {
+	param(
+		[string]$hostname
+	)
+	$hostname=$hostname.ToLower();
+	$count=0;
+	foreach ($vm in $global:VMLIST) {
+        $VMhostname=getVMHostname $vm;
+		if ($VMhostname -and ($hostname -eq $VMhostname.ToLower())) {
+			$count++;
+		}
+	}
+	return $count;
+}
+
+
+#посчитать 
+function countVMsHostnameIp() {
+	param(
+		[string]$hostname,
+        $ips
+	)
+	$hostname=$hostname.ToLower();
+	$count=0;
+	:vms foreach ($vm in $global:VMLIST) {
+        $VMhostname=getVMHostname $vm;
+		if ($VMhostname -and ($hostname -eq $VMhostname.ToLower())) {
+            $VMips = getVMIps $vm;
+            foreach ($ip in $ips) {
+                if ( -not ($VMips -contains $ip)) {
+                    continue vms;
+                }
+            }
+			$count++;
+		}
+	}
+	return $count;
+}
+
+
+function getVMUUID($vm) {
+    return "$($vm.PersistentId)@$($vm.Uid.Substring($vm.Uid.IndexOf('@')+1).Split(":")[0])"
+}
+
+function getVMhostUUID($vmhost) {
+    return "$( (Get-VMhost $vmhost.name |Get-View).hardware.systeminfo.uuid )@$($vmhost.Uid.Substring($vmhost.Uid.IndexOf('@')+1).Split(":")[0])"
 }
--- powercli.ps1
+++ powercli.ps1
@@ -4,11 +4,12 @@
 )
 
 $ScriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
-. $ScriptDir\lib_funcs.ps1
-. $ScriptDir\lib_inventory.ps1
-. $ScriptDir\lib_powercli3.ps1
+. $ScriptDir\..\libs.ps1\lib_funcs.ps1
+. $ScriptDir\..\libs.ps1\lib_inventory.ps1
+. $ScriptDir\lib_powercli.ps1
 . $ScriptDir\config.priv.ps1
-#$DEBUG_MODE=0
+$global:DEBUG_MODE=0;
+$global:InvRequestTimeout=30000;
 
 $script:logfile=	"$($ScriptDir)\powercli_inventory.log"
 $script:errorLogfile=	"$($ScriptDir)\powercli_inventory_error.log"
@@ -17,42 +18,102 @@ $script:VMerrorListFile="$($ScriptDir)\powercli_inventory_error.VM"
 $script:errorFlagFile=	"$($ScriptDir)\powercli_inventory_error.flg"
 
 $script:scriptErrorsArray=[ordered]@{}
-$script:scriptErrorsFlag=0
+$script:scriptErrorsFlag=0;
 
+$global:skip404errors=$true;
+
+$global:VMLIST=@();
+$global:VMGuests=@{};
+
+
+
+spooLog "Loading inventory data...";
+inventoryCacheAllComps;
+
+spooLog "Loading VMWare data"
 
 foreach ($item in $vCentersList) {
 	$node=$item.node;		#fqdn
 	$name=$node.split(".")[0];	#hostname
-	if ($vcenter.length) {
-		if ( ($vcenter -eq $node) -or ($vcenter -eq $name)) {
-			if ($vm.length) {
-				parseVCenterVm $item.node $item.login $item.password $vm
+	if ($vcenter.length) {		#если у нас ограничение на конкретный vcenter
+		if ( ($vcenter -eq $node) -or ($vcenter -eq $name)) {			
+			connectVCenter $item.node $item.login $item.password
+		}
+	} else {
+		connectVCenter $item.node $item.login $item.password
+	}
+}
+
+if ($vm.length) {
+	parseVCenter $vm
+} else {
+	parseVCenter
+}
+
+spooLog "$( $VMLIST.length ) running VMs loaded."
+
+
+
+$VMLIST
+#$VMGuests;
+
+foreach ($_ in $VMLIST) {
+#	$_|fl;
+	$hostname=getVMHostname($_);
+	#$([string]$hostname).toLower();
+	#if (-not(([string]$hostname).toLower() -eq 'erp-prod')) {continue};
+	$uuid=getVMUUID $_;
+	$invHost=searchInvByVMUUID($uuid);
+	if ($invHost -eq $false ){
+		$VMcount=countVMsHostname($hostname);
+		$Invcount=countInventoryFqdnComps($hostname);
+		#spooLog "$hostname : $VMcount : $Invcount"
+		#у нас одна такая ВМ и не более одного узла в инвентори
+		if (($VMcount -eq 1) -and ($Invcount -le 1)) {
+			pushVMData ($_);
+		} else {
+			$ips = getVMIps $_;
+			if ($ips.length -gt 0) {
+				$VMcount=countVMsHostnameIp $hostname $ips;
+				$Invcount=countInventoryFqdnIPComps $hostname $ips;
+				if (($VMcount -eq 1) -and ($Invcount -eq 1)) {
+					pushVMData $_ $(getInventoryFqdnIpComp $hostname $ips);
+				} else {
+					if ( -not $hostname) {$hostname='<no hostname>'}
+					errorLog "VM: $($hostname) (VM: $($_.Name); UUID: $($uuid); IPS: $($ips -join ' ')) не найдена в инвентори по UUID, и не идентифицируется по hostname+IP ($VMcount ВМ, $Invcount ОС)"
+				}
 			} else {
-				parseVCenter $item.node	$item.login $item.password
+				if ( -not $hostname) {$hostname='<no hostname>'}
+				errorLog "VM: $($hostname) (VM: $($_.Name); UUID: $($uuid); IPS: <missing>) не найдена в инвентори по UUID, и не идентифицируется по hostname ($VMcount ВМ, $Invcount ОС)"              	
 			}
 		}
 	} else {
-		parseVCenter $item.node	$item.login $item.password
+		debugLog "$hostname found by VM UUID"
+		pushVMData $_ $invHost;
 	}
 }
 
-exit
+
+Disconnect-VIServer -server * -force  -confirm:$false |Out-Null
+
+
 $scriptErrorsFlag | Out-File -filePath $errorFlagFile
 
-$errList="";
-$VMerrList="";
+$errList=@();
+$VMerrList=@();
 foreach($err in $scriptErrorsArray.GetEnumerator()) {
-	$errList= -join ($errList, $err.Value,"x :",$err.Name,"`n");
+	$errList+= -join ($err.Value,"x :",$err.Name);
 	if ($err.Name.StartsWith('VM:')) {
-		$VMerrList= -join ($VMerrList, $err.Value,"x :",$err.Name,"`n");
+		$VMerrList+= -join ($err.Value,"x :",$err.Name);
 	}
 }
-$errList | Out-File -filePath $errorListFile -encoding Default
-$VMerrList | Out-File -filePath $VMerrorListFile -encoding Default
+
+$($errList | Sort-Object)   -join "`n" | Out-File -filePath $errorListFile -encoding Default
+$($VMerrList | Sort-Object) -join "`n" | Out-File -filePath $VMerrorListFile -encoding Default
 
 if ($scriptErrorsFlag) {
-	Log "WARNING: script complete with errors!"
+	spooLog "WARNING: script complete with errors!"
 	$scriptErrorsArray
 } else {
-	Log "Script complete with no errors."
+	spooLog "Script complete with no errors."
 }
